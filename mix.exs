dbe3bce9e34fbd30ff7028078516f7471c855d6c
ericdude4 <eric_froese2@hotmail.com>
2020-05-11 15:21:54 -0400

    inc veresion

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Stories.MixProject do
   def project do
     [
       app: :stories,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
